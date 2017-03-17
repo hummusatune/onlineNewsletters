598baabc3a08c5ddb15d730fb24baf0df1d17aaa
Mike Mitchell <mmitchell@weber.edu>
2017-03-17 13:25:24 -0600

    Evolution of newsletters to Foundation

---
+++ fall/js/app.js
@@ -0,0 +1,9 @@
+//First hide all of the content
+$(document).ready(function(){
+  $(".hover-reveal").hide();
+});
+
+//Now make the content pop up with a click function
+$(".section-title").click(function() {
+  $(this).next('.hover-reveal').slideToggle();
+});
